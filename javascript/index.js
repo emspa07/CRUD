329b8d7c78e4f548a295884b8132b6573c174cf8
emspa07 <empaulin.dwfsc14@bootcamp.ucamp.io>
2023-02-07 18:25:15 -0600

    editar y borrar tareas en js

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,8 @@ let nombreInput = document.getElementById('nombreInput')
 let fechaInput = document.getElementById('fechaInput')
 let textareaDescrip = document.getElementById ('textareaDescrip')
 let message = document.getElementById ('message')
+let metas = document.getElementById('metas')
+let agregar = document.getElementById ('agregar')
 
 form.addEventListener('submit', (evt)=>{
   evt.preventDefault()
@@ -18,6 +20,14 @@ let formValidation = ()=>{
     console.log('valid')
     message.innerHTML = ""
     acceptData()
+    agregar.setAttribute("data-bs-dismiss", "modal")
+    agregar.click()
+
+
+    (() =>{
+      agregar.setAttribute("data-bs-dismiss", "")
+    })()
+
   }
 }
 
@@ -28,5 +38,38 @@ let acceptData = () => {
   data["date"] = fechaInput.value
   data["description"] = textareaDescrip.value 
 
-  console.log(data)
+  createMetas()
+}
+
+let createMetas = ()=>{
+  metas.innerHTML += ` <div>
+  <span class="task-me">${data.text}</span>
+  <span class="date">${data.date}</span> 
+  <p>${data.description}</p>
+  <span class="opciones">
+      <i onClick ="editMeta(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-regular fa-pen-to-square"></i><i onClick = "deleteMeta(this)" class="fa-solid fa-trash-can"></i>
+  </span>
+  </div>`
+
+  resetForm();
+}
+
+let deleteMeta = (e)=>{  
+  e.parentElement.parentElement.remove ()
+}
+
+let editMeta = (e)=>{
+  let selectedMeta = e.parentElement.parentElement
+
+  nombreInput.value = selectedMeta.children[0].innerHTML
+  fechaInput.value = selectedMeta.children[1].innerHTML
+  textareaDescrip.value = selectedMeta.children[2].innerHTML
+
+  selectedMeta.remove()
+}
+
+let resetForm = () => {
+  nombreInput.value = ""
+  fechaInput.value = ""
+  textareaDescrip.value = ""
 }
